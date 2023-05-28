7a4229154029dfa363dca082d3a2c6cac48ca034
Fajri Fath <vipercode305@gmail.com>
2023-05-28 16:53:41 +0700

    change field type opened_at and closed_at to time.Time

--- models/courses.go
+++ models/courses.go
@@ -8,19 +8,19 @@ import (
 
 // Course is type for implemnting course model.
 type Course struct {
-	ID          int            `gorm:"primaryKey" json:"id,omitempty"`
-	Title       string         `gorm:"size:50;unique" json:"title,omitempty"`
-	Logo        string         `gorm:"size:255;null" json:"logo,omitempty"`
-	Description string         `gorm:"size:255" json:"description,omitempty"`
-	Content     string         `gorm:"type:text" json:"content,omitempty"`
-	IsActive    bool           `gorm:"default:false" json:"is_active,omitempty"`
-	OpenedAt    string         `gorm:"size:10" json:"opened_at,omitempty"`
-	ClosedAt    string         `gorm:"size:10" json:"closed_at,omitempty"`
-	UpdatedAt   time.Time      `json:"updated_at,omitempty"`
-	CreatedAt   time.Time      `json:"created_at,omitempty"`
-	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
-	Mentors     []*User        `gorm:"many2many:courses_users_mentor" json:"mentors,omitempty"`
-	Members     []*User        `gorm:"many2many:courses_users_member" json:"members,omitempty"`
+	ID          int            `gorm:"primaryKey" json:"id"`
+	Title       string         `gorm:"size:50;unique" json:"title"`
+	Logo        string         `gorm:"size:255;null" json:"logo"`
+	Description string         `gorm:"size:255" json:"description"`
+	Content     string         `gorm:"type:text" json:"content"`
+	IsActive    bool           `gorm:"default:false" json:"is_active"`
+	OpenedAt    time.Time      `gorm:"size:10" json:"opened_at"`
+	ClosedAt    time.Time      `gorm:"size:10" json:"closed_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	CreatedAt   time.Time      `json:"created_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+	Mentors     []*User        `gorm:"many2many:courses_users_mentor" json:"mentors"`
+	Members     []*User        `gorm:"many2many:courses_users_member" json:"members"`
 }
 
 // CreateNewCourse is function for create new course.
